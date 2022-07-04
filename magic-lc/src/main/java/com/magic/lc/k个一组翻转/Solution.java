e14a2faca1665c9cc5d00d1bf39daa6b52ed352f
ChengYufei <873401580@qq.com>
2022-07-04 22:34:40 +0800

    lc - k组翻转

---
+++ magic-lc/src/main/java/com/magic/lc/k个一组翻转/Solution.java
@@ -0,0 +1,68 @@
+package com.magic.lc.k个一组翻转;
+
+import com.magic.lc.反转链表.ListNode;
+
+/**
+ *
+ * @author Cheng Yufei
+ * @create 2022-07-04 21:42
+ **/
+public class Solution {
+
+    public static void main(String[] args) {
+        //ListNode node4 = new ListNode(4, null);
+
+        ListNode node3 = new ListNode(3, null);
+
+        ListNode node2 = new ListNode(2, node3);
+
+        ListNode node1 = new ListNode(1, node2);
+
+        // 1 -> 2 -> 3 ->4
+        int k = 2;
+        ListNode result = new ListNode();
+        result.next = node1;
+        ListNode pre = result;
+        while (node1 != null) {
+            ListNode tail = pre;
+            for (int i = 0; i < k; i++) {
+                tail = tail.next;
+                if (tail == null) {
+                    print(result);
+                    return;
+                }
+            }
+            ListNode next = tail.next;
+            ListNode reverse = reverse(node1, tail);
+            pre.next = reverse;
+            reverse.next = next;
+            pre = tail;
+            node1 = next;
+
+        }
+        print(result);
+
+    }
+
+    public static ListNode reverse(ListNode head, ListNode tail) {
+        ListNode pre = null;
+        while (head != tail) {
+            ListNode tmp = head.next;
+            head.next = pre;
+            pre = head;
+            head = tmp;
+        }
+        print(pre);
+        return pre;
+    }
+
+    public static void print(ListNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.getValue());
+        print(node.next);
+
+    }
+
+}
