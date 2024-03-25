4d4c96e75c4b97714692c3c75e97bbfa93e09209
CYF <873401580@qq.com>
2024-03-25 23:06:52 +0800

    activiti:  启动流程实例时，参与表

--- magic-activiti/src/main/java/com/magic/activiti/service/ActivitiService.java
+++ magic-activiti/src/main/java/com/magic/activiti/service/ActivitiService.java
@@ -42,6 +42,19 @@ public class ActivitiService {
     }
 
 
+    /**
+     * 启动流程实例，参与表：
+     *  act_hi_actinst：流程实例执行历史，一个 PROC_INST_ID_ 对应多条数据，包括：startEvent -> userTask 等 一个流程上的节点，并包含每个节点的startTime、endTime、assignee\
+     *  act_hi_identitylink: 流程实例参与用户历史信息,
+     *  act_hi_procinst： 流程实例历史信息，一个 PROC_INST_ID_ 对应一条数据，一个流程整体的 startTime、 endTime
+     *  act_hi_taskinst： 流程任务历史信息
+     *
+     *  act_ru_execution ： 流程正在执行的信息，可有 ACT_ID_ 关联到 act_hi_actinst
+     *  act_ru_identitylink：流程的参与用户信息
+     *  act_ru_task:   流程正在执行的任务信息
+     *
+     * @return
+     */
     public String startProcess() {
         //key: ACT_RE_PROCDEF # KEY_  字段
         ProcessInstance myEvectionProcessInstance = runtimeService.startProcessInstanceByKey("myEvection");
