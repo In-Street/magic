8d286350ec43031fe54df6311340d4b1feb3a06f
CYF <873401580@qq.com>
2025-01-30 15:35:18 +0800

    final修饰成员变量：基本类型、字符串、引用类型； final修饰方法：不可被子类重写

--- magic-time/src/main/java/com/magic/time/service/jvm/class_load/Parent.java
+++ magic-time/src/main/java/com/magic/time/service/jvm/class_load/Parent.java
@@ -11,8 +11,23 @@ public class Parent {
     }
 
     static final String parentParam = "父类final静态变量";
-    static  String no_final_parent_param = "父类静态变量";
+    static String no_final_parent_param = "父类静态变量";
 
-    static  int[] parent_int_array= {1,2};
+    /**
+     *  final 修饰的成员变量：
+     *      基本类型、字符串： 值不可变， 重新赋值时会编译错误；
+     *
+     *      引用类型：引用不能指向另一个对象，但对象内部的状态可以改变；
+     */
+    final int basic_int = 10;
+    final String basic_string = "AAA";
+    final StringBuilder sb = new StringBuilder("CCC");
 
+
+
+    static int[] parent_int_array = {1, 2};
+
+    protected   Object getA(Integer a) {
+        return "Parent getA";
+    }
 }
--- magic-time/src/main/java/com/magic/time/service/jvm/class_load/TestClassLoad.java
+++ magic-time/src/main/java/com/magic/time/service/jvm/class_load/TestClassLoad.java
@@ -9,24 +9,29 @@ import java.lang.reflect.Field;
  **/
 public class TestClassLoad {
 
-    public static void main(String[] args) throws NoSuchFieldException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws NoSuchFieldException, InstantiationException, IllegalAccessException, ClassNotFoundException {
         // 访问final修饰的 基本类型/字符串 不会触发类的初始化
-        // System.out.println(Child.parentParam);
+        System.out.println(Child.parentParam);
 
         // 子类访问父类的非 final静态变量会触发父类的初始化，不会触发子类的初始化
         // System.out.println(Child.no_final_parent_param);
 
         //数组定义类，不会触发类的初始化
-        // Child[] childArray = new Child[2];
+        Child[] childArray = new Child[2];
 
         //单纯的获取Field 不会触发初始化。 调用get访问静态字段会触发初始化【即使是final修饰的基本类型/字符串静态字段】
         Field declaredField = Parent.class.getDeclaredField("no_final_parent_param");
-        System.out.println(declaredField.get(null));
+        // System.out.println(declaredField.get(null));
 
         //单纯的获取引用，不会触发初始化
         Class<Parent> parentClass = Parent.class;
         System.out.println(parentClass.getName());
+
+        // newInstance： 会触发类的初始化
         // Parent parent = parentClass.newInstance();
+
+        //会触发类的初始化
+        Class<?> aClass = Class.forName("com.magic.time.service.jvm.class_load.Child");
 
 
     }
