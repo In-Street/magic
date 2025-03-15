a1bbd57ecd91449e15191cce36b75f10e8b04f43
CYF <873401580@qq.com>
2025-03-15 23:24:34 +0800

    单个切面的执行顺序、多个切面的执行顺序 【magic-time  AopConfig.java 、HighAopConfig.java】

--- magic-time/src/main/java/com/magic/time/service/business_development_100/spring_19/HighAopConfig.java
+++ magic-time/src/main/java/com/magic/time/service/business_development_100/spring_19/HighAopConfig.java
@@ -3,14 +3,13 @@ package com.magic.time.service.business_development_100.spring_19;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
- *  单个切面的执行顺序：
- * @Around  -  @Before  -  目标方法执行  -  @AfterReturning / @AfterThrowing   -  @After  -  @Around
  *
+ *  多个切面作用于同一连接点时，按优先级顺序执行：
+ *    High Around  -  High Before  - Low Around  -  Low Before  -  目标方法执行  -  Low AfterReturning  -  Low After  -  Low  Around  -  High AfterReturning  -  High After  -  High  Around
  *
  * @author Cheng Yufei
  * @create 2022-05-15 18:56
@@ -18,32 +17,32 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @Slf4j
-// 低优先级切面
-@Order(2)
-public class AopConfig {
+// 高优先级切面
+@Order(1)
+public class HighAopConfig {
 
     @Around("@annotation(org.springframework.web.bind.annotation.GetMapping)")
     public Object around(ProceedingJoinPoint pjp) throws Throwable {
-        log.info(">>Around before");
+        log.info(">> High Around before");
         Object proceed = pjp.proceed();
-        log.info("<< Around after");
+        log.info("<< High Around after");
         return proceed;
     }
 
 
     @Before("@annotation(org.springframework.web.bind.annotation.GetMapping)")
     public void before() {
-        log.info(">> before");
+        log.info(">>High before");
     }
 
     @After("@annotation(org.springframework.web.bind.annotation.GetMapping)")
     public void after() {
-        log.info("<< after");
+        log.info("<< High after");
     }
 
     @AfterReturning(returning = "result", pointcut = "@annotation(org.springframework.web.bind.annotation.GetMapping)")
     public void afterReturn(Object result) {
-        log.info(">> After Returning,{}", result);
+        log.info(">>High  After Returning,{}", result);
     }
 
 }
